f8cf5fb1cbae5c93cb372905c61daae4f01419e0
Fredrik Andersson <f.anderzon@gmail.com>
2016-08-06 12:19:30 +0200

    Development bundle set up

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import buble from 'rollup-plugin-buble';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+	entry: 'bin/src/index.js',
+	dest: 'bin/lambdasync',
+	format: 'cjs',
+	banner: '#!/usr/bin/env node',
+	plugins: [
+		buble(),
+		commonjs({
+			include: 'node_modules/**'
+		}),
+		nodeResolve({
+			main: true
+		})
+	],
+	external: [
+		'fs',
+		'path',
+		'module',
+		'source-map-support'
+	]
+};
