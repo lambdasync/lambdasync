21bd5ebee5b744f63b9adc7899b28d0acbca6a85
Fredrik Andersson <f.anderzon@gmail.com>
2016-08-06 23:28:19 +0200

    Build watch added

--- rollup.config.js
+++ rollup.config.js
@@ -3,23 +3,24 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
-	entry: 'bin/src/index.js',
-	dest: 'bin/lambdasync',
-	format: 'cjs',
-	banner: '#!/usr/bin/env node',
-	plugins: [
-		buble(),
-		commonjs({
-			include: 'node_modules/**'
-		}),
-		nodeResolve({
-			main: true
-		})
-	],
-	external: [
-		'fs',
-		'path',
-		'module',
-		'source-map-support'
-	]
+  entry: 'bin/src/index.js',
+  dest: 'bin/lambdasync',
+  format: 'cjs',
+  banner: '#!/usr/bin/env node',
+  plugins: [
+    buble(),
+    commonjs({
+      include: 'node_modules/**',
+      exclude: 'node_modules/acorn/**'
+    }),
+    nodeResolve({
+      main: true
+    })
+  ],
+  external: [
+    'fs',
+    'path',
+    'module',
+    'source-map-support'
+  ]
 };
