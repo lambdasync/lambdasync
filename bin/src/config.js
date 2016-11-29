0eb6f4fa217432b82dcc0aa6af8da68b1bb27ed5
Fredrik Andersson <f.anderzon@gmail.com>
2016-11-29 23:46:25 +0100

    Pretty print the current config

--- bin/src/config.js
+++ bin/src/config.js
@@ -1,34 +1,41 @@
 const aws = require('./aws.js');
-const {awsPromise} = require('./util.js');
+const {awsPromise, parseCommandArgs, markdown} = require('./util.js');
 
 function config(settings, args) {
   const argCount = Array.isArray(args) ? args.length : 0;
 
   const AWS = aws(settings);
   const api = new AWS.Lambda();
+  const requestParams = {
+    FunctionName: settings.lambdaArn
+  };
 
-  // No args, just print all config
-  if (argCount === 0) {
-    awsPromise(api, 'getFunctionConfiguration', {
-      FunctionName: settings.lambdaArn
-    })
-      .then(res => console.log(res))
-      .catch(err => console.log(err));
+  function configToLambdaConfig(config, defaultValues = {}) {
+    let values = defaultValues;
+
   }
 
-  // One arg, we'll try to show one specific piece of config
-  if (argCount === 1) {
-    awsPromise(api, 'getFunctionConfiguration', {
-      FunctionName: settings.lambdaArn
+  // There are 2 things that could be happening
+  // `lambdasync config` no args, print all config
+  // `lambdasync config x=10 y=foo` set the x config to 10, y to 'foo'
+  // (any keys without values will be ignored)
+  // Regardless we first need the current config
+
+  awsPromise(api, 'getFunctionConfiguration', requestParams)
+    .then(currentConfig => {
+      if (argCount === 0) {
+        console.log(markdown({
+          templatePath: 'markdown/config.md',
+          data: currentConfig
+        }));
+      }
+
+      const parsedArgs = parseCommandArgs(args, settings);
+      console.log('parsedArgs', parsedArgs);
+
+
     })
-      .then(res => {
-        console.log(res[args[0]]);
-      })
-      .catch(err => console.log(err));
-  }
-
-
-  console.log(args);
+    .catch(err => console.log(err));
 }
 
 module.exports = config;
