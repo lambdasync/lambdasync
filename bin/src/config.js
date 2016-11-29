c8aef47c4eabe011d6df736b8671704d20f653e7
Fredrik Andersson <f.anderzon@gmail.com>
2016-11-29 23:05:09 +0100

    Naive implementation of show all config, and show one piece of config

--- bin/src/config.js
+++ bin/src/config.js
@@ -1,4 +1,33 @@
+const aws = require('./aws.js');
+const {awsPromise} = require('./util.js');
+
 function config(settings, args) {
+  const argCount = Array.isArray(args) ? args.length : 0;
+
+  const AWS = aws(settings);
+  const api = new AWS.Lambda();
+
+  // No args, just print all config
+  if (argCount === 0) {
+    awsPromise(api, 'getFunctionConfiguration', {
+      FunctionName: settings.lambdaArn
+    })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }
+
+  // One arg, we'll try to show one specific piece of config
+  if (argCount === 1) {
+    awsPromise(api, 'getFunctionConfiguration', {
+      FunctionName: settings.lambdaArn
+    })
+      .then(res => {
+        console.log(res[args[0]]);
+      })
+      .catch(err => console.log(err));
+  }
+
+
   console.log(args);
 }
 
