8fdff200e2ea027d9b99f523488aaa6250e39328
Fredrik Andersson <f.anderzon@gmail.com>
2017-01-08 00:23:57 +0100

    Tested devserver apps using express and patch and delete methods

--- bin/src/devserver/index.test.js
+++ bin/src/devserver/index.test.js
@@ -1,4 +1,5 @@
-const request = require('supertest-as-promised');
+console.warn = () => {};
+const request = require('supertest');
 const devserver = require('./index').setup;
 const settingsMock = require('../../test/settings-mock.js');
 const handlerMock = require('../../test/handler-mock.js');
@@ -41,6 +42,48 @@ describe('Devserver', () => {
       .then((res) => {
         expect(typeof res.body).toBe('string');
         expect(res.body).toBe('Everything is awesome!');
+      });
+  });
+
+  it('can handle POST', () => {
+    const app = devserver(settingsMock.fullSettings, handlerMock.usersPostWithBody);
+    return request(app)
+      .post('/users')
+      .send({id:3,name:'Lambda'})
+      .expect(200)
+      .then((res) => {
+        expect(typeof res.body).toBe('object');
+        expect(res.body.name).toBe('Lambda');
+      });
+  });
+
+  it('can handle PATCH', () => {
+    const app = devserver(settingsMock.fullSettings, handlerMock.usersPatchWithBody);
+    return request(app)
+      .patch('/users/1')
+      .send({name:'Lambda'})
+      .expect(200)
+      .then((res) => {
+        expect(typeof res.body).toBe('object');
+        expect(res.body.name).toBe('Lambda');
+      });
+  });
+
+  it('can handle PATCH', () => {
+    const app = devserver(settingsMock.fullSettings, handlerMock.usersDelete);
+    return request(app)
+      .delete('/users/1')
+      .expect(200);
+  });
+
+  it('can handle express apps', () => {
+    const app = devserver(settingsMock.fullSettings, handlerMock.expressApp());
+    return request(app)
+      .get('/users')
+      .expect(200)
+      .then((res) => {
+        expect(Array.isArray(res.body)).toBeTruthy();
+        expect(res.body[0].name).toBe('Joe');
       });
   });
 });
--- bin/test/handler-mock.js
+++ bin/test/handler-mock.js
@@ -5,7 +5,7 @@ const defaultHeaders = {
   'Content-Type': 'application/json'
 };
 
-function createProxyReturnObject(statusCode, body, headers = defaultHeaders) {
+function createProxyReturnObject(statusCode, body = null, headers = defaultHeaders) {
   return {
     statusCode,
     headers,
@@ -29,8 +29,105 @@ function callback(event, context, callback) {
   callback(createProxyReturnObject(200, 'Everything is awesome!'));
 };
 
+function usersPostWithBody(event, context, callback) {
+  if (event.httpMethod === 'POST' && event.path === '/users') {
+    context.succeed(createProxyReturnObject(200, JSON.parse(event.body)));
+  }
+  return context.fail(createProxyReturnObject(500, {error: 'This test failed.'}));
+};
+
+function usersPatchWithBody(event, context, callback) {
+  if (event.httpMethod === 'PATCH' && event.path === '/users/1') {
+    context.succeed(createProxyReturnObject(200, JSON.parse(event.body)));
+  }
+  return context.fail(createProxyReturnObject(500, {error: 'This test failed.'}));
+};
+
+function usersDelete(event, context, callback) {
+  if (event.httpMethod === 'DELETE' && event.path === '/users/1') {
+    context.succeed(createProxyReturnObject(200));
+  }
+  return context.fail(createProxyReturnObject(500, {error: 'This test failed.'}));
+};
+
+function expressApp() {
+  const awsServerlessExpress = require('aws-serverless-express');
+  const express = require('express');
+  const bodyParser = require('body-parser');
+  const cors = require('cors');
+  const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
+
+  const app = express();
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(awsServerlessExpressMiddleware.eventContext());
+
+  // Ephemeral in-memory data store
+  const users = [{id: 1, name: 'Joe'}, {id: 2, name: 'Jane'}];
+  let userIdCounter = users.length;
+
+  // User functions
+  const getUser = userId => users.find(u => u.id === parseInt(userId, 10));
+  const getUserIndex = userId => users.findIndex(u => u.id === parseInt(userId, 10));
+
+  app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+  });
+
+  app.get('/users', (req, res) => {
+    res.json(users);
+  });
+
+  app.get('/users/:userId', (req, res) => {
+    const user = getUser(req.params.userId);
+    if (!user) {
+      return res.status(404).json({});
+    }
+
+    return res.json(user);
+  });
+
+  app.post('/users', (req, res) => {
+    const user = {
+      id: ++userIdCounter,
+      name: req.body.name
+    };
+    users.push(user);
+    res.status(201).json(user);
+  });
+
+  app.put('/users/:userId', (req, res) => {
+    const user = getUser(req.params.userId);
+    if (!user) {
+      return res.status(404).json({});
+    }
+
+    user.name = req.body.name;
+    res.json(user);
+  });
+
+  app.delete('/users/:userId', (req, res) => {
+    const userIndex = getUserIndex(req.params.userId);
+    if (userIndex === -1) {
+      return res.status(404).json({});
+    }
+
+    users.splice(userIndex, 1);
+    res.json(users);
+  });
+
+  const server = awsServerlessExpress.createServer(app);
+
+  return (event, context) => awsServerlessExpress.proxy(server, event, context);
+}
+
 exports = module.exports = {};
 exports.contextSucceed = contextSucceed;
 exports.contextFail = contextFail;
 exports.contextDone = contextDone;
 exports.callback = callback;
+exports.usersPostWithBody = usersPostWithBody;
+exports.usersPatchWithBody = usersPatchWithBody;
+exports.usersDelete = usersDelete;
+exports.expressApp = expressApp;
