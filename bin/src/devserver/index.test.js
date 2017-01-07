770b9be4b3b915bb12ade480388d9d829e01b004
Fredrik Andersson <f.anderzon@gmail.com>
2017-01-07 23:38:04 +0100

    Added devserver tests for all lambda return callbacks

--- bin/src/devserver/index.test.js
+++ bin/src/devserver/index.test.js
@@ -4,13 +4,43 @@ const settingsMock = require('../../test/settings-mock.js');
 const handlerMock = require('../../test/handler-mock.js');
 
 describe('Devserver', () => {
-  it('basic request', () => {
-    const app = devserver(settingsMock.fullSettings, handlerMock.simpleSuccess);
+  it('can handle context.succeed', () => {
+    const app = devserver(settingsMock.fullSettings, handlerMock.contextSucceed);
     return request(app).get('/')
-    .expect(200)
-    .then((res) => {
-      expect(typeof res.body).toBe('string');
-      expect(res.body).toBe('Everything is awesome!');
-    });
+      .expect(200)
+      .then((res) => {
+        expect(typeof res.body).toBe('string');
+        expect(res.body).toBe('Everything is awesome!');
+      });
+  });
+
+  it('can handle context.fail', () => {
+    const app = devserver(settingsMock.fullSettings, handlerMock.contextFail);
+    return request(app).get('/')
+      .expect(404)
+      .then((res) => {
+        expect(typeof res.body).toBe('string');
+        expect(res.body).toBe('Oops! Not found.');
+      });
+  });
+
+  it('can handle context.done', () => {
+    const app = devserver(settingsMock.fullSettings, handlerMock.contextDone);
+    return request(app).get('/')
+      .expect(500)
+      .then((res) => {
+        expect(typeof res.body).toBe('object');
+        expect(res.body.error).toBe('This test failed.');
+      });
+  });
+
+  it('can handle callbacks', () => {
+    const app = devserver(settingsMock.fullSettings, handlerMock.callback);
+    return request(app).get('/')
+      .expect(200)
+      .then((res) => {
+        expect(typeof res.body).toBe('string');
+        expect(res.body).toBe('Everything is awesome!');
+      });
   });
 });
--- bin/test/handler-mock.js
+++ bin/test/handler-mock.js
@@ -1,14 +1,36 @@
-function simpleSuccess(event, context) {
-  context.succeed({
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify('Everything is awesome!')
-  });
+const users = [{id: 1, name: 'Joe'}, {id: 2, name: 'Jane'}];
+
+const defaultHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Content-Type': 'application/json'
 };
 
-module.exports = {
-  simpleSuccess
+function createProxyReturnObject(statusCode, body, headers = defaultHeaders) {
+  return {
+    statusCode,
+    headers,
+    body: JSON.stringify(body)
+  };
+}
+
+function contextSucceed(event, context) {
+  context.succeed(createProxyReturnObject(200, 'Everything is awesome!'));
 };
+
+function contextFail(event, context) {
+  context.fail(createProxyReturnObject(404, 'Oops! Not found.'));
+};
+
+function contextDone(event, context) {
+  context.done(createProxyReturnObject(500, {error: 'This test failed.'}));
+};
+
+function callback(event, context, callback) {
+  callback(createProxyReturnObject(200, 'Everything is awesome!'));
+};
+
+exports = module.exports = {};
+exports.contextSucceed = contextSucceed;
+exports.contextFail = contextFail;
+exports.contextDone = contextDone;
+exports.callback = callback;
