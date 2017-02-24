7c667dec6dce59e2148cfbe895670f99a0a8d59c
Fredrik Andersson <f.anderzon@gmail.com>
2017-02-24 08:46:04 +0100

    Test write and transform fails

--- bin/src/file.test.js
+++ bin/src/file.test.js
@@ -6,6 +6,7 @@ const rimraf = require('rimraf');
 const {readFile, writeFile} = require('./file');
 
 const TEST_DIR = path.join(process.cwd(), 'file-module-tests');
+const MISSING_DIR = path.join(process.cwd(), 'make-believe');
 const TEST_FILE_EXISTING = path.join(TEST_DIR, 'i-am-here.txt');
 const TEST_FILE_CREATE = path.join(TEST_DIR, 'create-me.txt');
 
@@ -23,7 +24,40 @@ describe('file', () => {
   describe('readFile', () => {
     it('expect reading missing file to fail', () => {
       readFile(TEST_FILE_CREATE)
-        .catch(e => expect(e).toBeTruthY());
+        .catch(err => expect(err).toBeTruthy());
+    });
+    it('expect reading existing file to work', () => {
+      readFile(TEST_FILE_EXISTING)
+        .then(res => expect(res).toBe('1'));
+    });
+    it('expect transforms to be applied', () => {
+      readFile(TEST_FILE_EXISTING, Number)
+        .then(res => expect(res).toBe(1));
+    });
+    it('expect failed transforms to reject', () => {
+      readFile(TEST_FILE_EXISTING, content => JSON.parse('{'+content))
+        .catch(err => expect(err).toBeTruthy());
+    });
+  });
+  describe('writeFile', () => {
+    const Ash = {klaatu: 'verata', neck: 'tie'};
+    it('expect writing file to succeed', () => {
+      writeFile(TEST_FILE_CREATE, Ash)
+        .then(res => expect(res).toBeTruthy());
+    });
+    it('expect transforms to be applied', () => {
+      writeFile(TEST_FILE_CREATE, Ash, JSON.stringify)
+        .then(res => expect(JSON.parse(res)).toEqual(Ash));
+    });
+    it('expect writing file to missing directory to fail', () => {
+      writeFile(path.join(MISSING_DIR, 'a-file.json'), Ash)
+        .catch(err => expect(err).toBeTruthy());
+    });
+    it('expect failed transforms to reject', () => {
+      const Williams = Object.assign({}, Ash);
+      Williams.Williams = Williams;
+      writeFile(TEST_FILE_CREATE, undefined, JSON.stringify)
+        .catch(err => expect(err).toBeTruthy());
     });
   });
 });
