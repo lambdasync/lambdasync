c6fcc70c0b0a5fbcb6ce4b77909736c8638ee264
Fredrik Andersson <f.anderzon@gmail.com>
2017-10-22 19:17:02 +0200

    Added `setupAutoScalingPolicy` function to set up auto scaling after table creation

--- bin/src/dynamodb.js
+++ bin/src/dynamodb.js
@@ -13,6 +13,69 @@ const {
 const {updateSettings, getSettings} = require('./settings');
 const {setupDynamoDbTablePolicy} = require('./iam');
 
+const ServiceNamespace = 'dynamodb';
+
+function getTableFromSettings(settings, tableName) {
+  return settings && settings.dynamoDbTables.find(
+    t => t.tableName === tableName);
+}
+
+function setupAutoScalingPolicy(settings, tableName) {
+  const AWS = aws(settings);
+  const api = new AWS.ApplicationAutoScaling();
+
+  return registerScalableTarget(api, settings, tableName)
+    .then(() => Promise.all([
+      awsPromise(api, 'putScalingPolicy', {
+        PolicyName: `DynamoDBReadCapacityUtilization:table/${tableName}`,
+        PolicyType: 'TargetTrackingScaling',
+        ResourceId: `table/${tableName}`,
+        ScalableDimension: 'dynamodb:table:ReadCapacityUnits',
+        ServiceNamespace,
+        TargetTrackingScalingPolicyConfiguration: {
+          TargetValue: 70.0, // Percent Limit of when to scale
+          PredefinedMetricSpecification: {
+            PredefinedMetricType: 'DynamoDBReadCapacityUtilization',
+          }
+        },
+      }),
+      awsPromise(api, 'putScalingPolicy', {
+        PolicyName: `DynamoDBWriteCapacityUtilization:table/${tableName}`,
+        PolicyType: 'TargetTrackingScaling',
+        ResourceId: `table/${tableName}`,
+        ScalableDimension: 'dynamodb:table:WriteCapacityUnits',
+        ServiceNamespace,
+        TargetTrackingScalingPolicyConfiguration: {
+          TargetValue: 70.0, // Percent Limit of when to scale
+          PredefinedMetricSpecification: {
+            PredefinedMetricType: 'DynamoDBWriteCapacityUtilization',
+          }
+        },
+      }),
+    ]));
+}
+
+function registerScalableTarget(api, settings, tableName) {
+  return Promise.all([
+    awsPromise(api, 'registerScalableTarget', {
+      MinCapacity: 5,
+      MaxCapacity: 10000,
+      ResourceId: `table/${tableName}`,
+      RoleARN: `arn:aws:iam::${settings.accountId}:role/service-role/DynamoDBAutoscaleRole`,
+      ScalableDimension: 'dynamodb:table:ReadCapacityUnits',
+      ServiceNamespace,
+    }),
+    awsPromise(api, 'registerScalableTarget', {
+      MinCapacity: 5,
+      MaxCapacity: 10000,
+      ResourceId: `table/${tableName}`,
+      RoleARN: `arn:aws:iam::${settings.accountId}:role/service-role/DynamoDBAutoscaleRole`,
+      ScalableDimension: 'dynamodb:table:WriteCapacityUnits',
+      ServiceNamespace,
+    })
+  ]);
+}
+
 function createDynamoDbTable(settings, tableName) {
   if (!tableName) {
     return Promise.reject('You need to specify a table name');
@@ -56,6 +119,8 @@ function handleTableCommand(settings, tableName) {
       return updateSettings({
         dynamoDbTables: tables
       })
+        .then(getSettings)
+        .then(settings => setupAutoScalingPolicy(settings, tableName))
         .then(() => data);
     });
 }
